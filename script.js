0e556fdb68b3cd0bedf1acfb1df7ec05271238ea
Surprisedgolf <maicol0983re@gmail.com>
2025-03-30 00:53:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.classList.add("header-fijo");
+        header.style.left = "50%";
+        header.style.transform = "translateX(-50%)"; 
+    } 
+    else {
+        header.classList.remove("header-fijo");
+        header.style.left = "50%";
+        header.style.transform = "translateX(-50%)";
+    }
+});
+
+
+const circle = document.getElementById("circle");
+const logo = document.getElementById("logo");
+
+const platforms = [
+    { src: "imagenes/xbox-logo-negro.png", color: "#107C10" },
+    { src: "imagenes/playstation-png-logo.png", color: "#003791" },
+    { src: "imagenes/nintendo.png", color: "#E60012" },
+    { src: "imagenes/steam.png", color: "#171A21" }
+];
+
+let index = 0;
+let interval = setInterval(changePlatform, 5000);
+
+function changePlatform() {
+    logo.style.transition = "opacity 0.8s ease-in-out"; 
+    logo.style.opacity = "0"; 
+
+    setTimeout(() => {
+        index = (index + 1) % platforms.length;
+        logo.src = platforms[index].src;
+        circle.style.background = platforms[index].color;
+        logo.style.opacity = "1"; 
+    }, 800);
+}
+
+
+circle.addEventListener("mouseover", () => {
+    logo.style.animationPlayState = "paused";
+    clearInterval(interval);
+});
+
+
+circle.addEventListener("mouseleave", () => {
+    logo.style.animationPlayState = "running";
+    interval = setInterval(changePlatform, 5000);
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const dosbox = document.getElementById("dosbox");
+    if (!dosbox) {
+        console.error("No se encontró el elemento #dosbox");
+        return;
+    }
+
+    try {
+        const dos = await Dos(dosbox).run("doom/doom.jsdos", {
+            useTouch: true // Activa los controles táctiles
+        });
+    } catch (error) {
+        console.error("Error al ejecutar js-dos:", error);
+    }
+});
